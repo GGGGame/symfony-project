7d19245d70c34539a671e20928c5ade481e717cc
Davide Ricca <51412794+GGGGame@users.noreply.github.com>
2022-09-26 10:48:49 +0200

    [#10] Test configuration
    * test configuration
    
    * Test configuration
    
    * disable test panther

---
+++ tests/Controller/ConferenceControllerTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Component\Panther\PantherTestCase;
+
+class ConferenceControllerTest extends PantherTestCase
+{
+    // public function testSomething(): void
+    // {
+    //     $client = static::createPantherClient();
+    //     $crawler = $client->request('GET', '/');
+
+    //     $this->assertResponseIsSuccessful();
+    //     $this->assertSelectorTextContains('h2', 'Give your feedback!');
+    // }
+
+    public function testCommentSubmission()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/conference/spain-2010');
+        $client->submitForm('Submit', [
+            'comment_form[author]' => 'davide',
+            'comment_form[text]' => 'text text',
+            'comment_form[email]' => 'test@gmail.com',
+            'comment_form[photo]' => dirname(__DIR__, 2).'/publicimages/under-construction.gif',
+
+        ]);
+        $this->assertResponseRedirects();
+        $client->followRedirect();
+        $this->assertSelectorExists('h2:contains("Conference")');
+    }
+    
+    public function testConferencePage()
+    {
+        $string = 'There are 1 comments.';
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertCount(2, $crawler->filter('h4'));
+
+        $client->clickLink('View');
+
+        $this->assertPageTitleContains('spain');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'spain 2010');
+        $this->assertSelectorExists('div:contains("comments")');
+    }
+}
